5417e2ffdb2ac47e1e8e4f218c0a1b9ca0fc3cec
Zoha Javed <zohajaved.098@gmail.com>
2025-06-18 23:59:58 +0500

    Header component

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import About from "./pages/About";
 import SignIn from "./pages/SignIn";
 import Profile from "./pages/Profile";
 import SignUp from "./pages/SignUp";
+import Header from "./components/header";
 
 export default function App() {
   return (
     <div>
       <Router>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
---
+++ src/components/Header.jsx
@@ -0,0 +1,45 @@
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <header className=" bg-slate-200 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <Link to={"/"}>
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap ">
+            <span className="text-blue-700 ">Zoha</span>
+            <span>Estate</span>
+          </h1>
+        </Link>
+        <form
+          action=""
+          className="flex items-center bg-slate-100 rounded-md p-3 "
+        >
+          <input
+            type="search"
+            name=""
+            id=""
+            className="bg-transparent outline-0 w-22 sm:w-60"
+            placeholder="Search..."
+          />
+          <FaSearch className="text-slate-700" />
+        </form>
+        <ul className="flex gap-4 ">
+          <Link to="/">
+            <li className="hidden sm:inline text-slate-800 hover:underline">
+              Home
+            </li>
+          </Link>
+          <Link to="/about">
+            <li className="hidden sm:inline text-slate-800 hover:underline">
+              About
+            </li>
+          </Link>
+          <Link to="/sign-in">
+            <li className="text-slate-800 hover:underline">Sign In</li>
+          </Link>
+        </ul>
+      </div>
+    </header>
+  );
+}
