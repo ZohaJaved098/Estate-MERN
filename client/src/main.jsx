05333f645d5f94ad78010232a18223e5c61ec9e3
Zoha Javed <zohajaved.098@gmail.com>
2025-06-24 22:17:32 +0500

    remove unnecessary comments

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,8 @@ import "./index.css";
 import App from "./App.jsx";
 import { persistor, store } from "./redux/store.js";
 import { Provider } from "react-redux";
-// import { PersistGate } from "redux-persist/es/integration/react";
-// import { PersistGate } from "redux-persist/lib/integration/react";
 import { PersistGate } from "redux-persist/integration/react";
-// PersistGate
-// PersistGate
+
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
