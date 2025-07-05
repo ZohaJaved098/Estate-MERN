afdca3da12046aab9b371988ecd0eb4b3b543fcd
Zoha Javed <zohajaved.098@gmail.com>
2025-07-05 19:36:39 +0500

    complete home page and about page

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -10,7 +10,7 @@ export default function ListingItem({ listing }) {
             listing.imageUrls[0] ||
             "https://53.fs1.hubspotusercontent-na1.net/hub/53/hubfs/Sales_Blog/real-estate-business-compressor.jpg?width=595&height=400&name=real-estate-business-compressor.jpg"
           }
-          alt="listing cover"
+          alt={listing.name || "listing cover"}
           className="h-[320px] sm:h-[220px] w-full object-cover hover:scale-105 transition-scale duration-300"
         />
         <div className="p-3 flex flex-col gap-2 w-full">
@@ -28,9 +28,9 @@ export default function ListingItem({ listing }) {
           </p>
           <p className="text-slate-500 mt-2 font-semibold ">
             $
-            {listing.offer
+            {listing.offer && listing.discountPrice
               ? listing.discountPrice.toLocaleString("en-US")
-              : listing.regularPrice.toLocaleString("en-US")}
+              : listing.regularPrice?.toLocaleString("en-US") || "N/A"}
             {listing.type === "rent" && " / month"}
           </p>
           <div className="text-slate-700 flex gap-4">
