2b61dbf9d0affe3d41faccb5cbb70218ebb0bb75
Zoha Javed <zohajaved.098@gmail.com>
2025-07-05 19:59:08 +0500

    add pre installation stuff

--- api/index.js
+++ api/index.js
@@ -8,7 +8,10 @@ import listingRouter from "./routes/listing.routes.js";
 
 import cookieParser from "cookie-parser";
 
+import path from "path";
+
 dotenv.config();
+const __dirname = path.resolve();
 const app = express();
 
 mongoose
@@ -29,6 +32,13 @@ app.use(cookieParser());
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("/{*any}", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
+
 //middleware for handling errors
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode ? err.statusCode : 500;
